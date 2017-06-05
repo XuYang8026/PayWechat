eedbc34eccbc3f7bb203aeae99307b664b2bef58
徐阳 <xuyang@yangs-macbook-pro.local>
2017-06-05 18:26:00 +0800

    扫一扫声音 音量放大

--- TLChat/SRC/Discover/ScannerViewController/TLScanningViewController.m
+++ TLChat/SRC/Discover/ScannerViewController/TLScanningViewController.m
@@ -312,26 +312,26 @@ static SystemSoundID shake_sound_male_id = 0;
 {
     NSString *path = [[NSBundle mainBundle] pathForResource:@"paysound" ofType:@"mp3"];
     
-    if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
-        NSURL *musicURL = [NSURL fileURLWithPath:path];
-        audioBtnMusicPlayer = [[AVAudioPlayer alloc]  initWithContentsOfURL:musicURL  error:nil];
-        audioBtnMusicPlayer.numberOfLoops = 0;//-1为一直循环
-        [audioBtnMusicPlayer setVolume:0.3];
-        [audioBtnMusicPlayer prepareToPlay];
-        [audioBtnMusicPlayer play];
-    }
-    
-    
-    
-    
-//    if (path) {
-//        //注册声音到系统
-//        AudioServicesCreateSystemSoundID((__bridge CFURLRef)[NSURL fileURLWithPath:path],&shake_sound_male_id);
-//        AudioServicesPlaySystemSound(shake_sound_male_id);
-//        //        AudioServicesPlaySystemSound(shake_sound_male_id);//如果无法再下面播放，可以尝试在此播放
+//    if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
+//        NSURL *musicURL = [NSURL fileURLWithPath:path];
+//        audioBtnMusicPlayer = [[AVAudioPlayer alloc]  initWithContentsOfURL:musicURL  error:nil];
+//        audioBtnMusicPlayer.numberOfLoops = 0;//-1为一直循环
+//        [audioBtnMusicPlayer setVolume:1];
+//        [audioBtnMusicPlayer prepareToPlay];
+//        [audioBtnMusicPlayer play];
 //    }
 //    
-//    AudioServicesPlaySystemSound(shake_sound_male_id);   //播放注册的声音，（此句代码，可以在本类中的任意位置调用，不限于本方法中）
+    
+    
+    
+    if (path) {
+        //注册声音到系统
+        AudioServicesCreateSystemSoundID((__bridge CFURLRef)[NSURL fileURLWithPath:path],&shake_sound_male_id);
+        AudioServicesPlaySystemSound(shake_sound_male_id);
+        //        AudioServicesPlaySystemSound(shake_sound_male_id);//如果无法再下面播放，可以尝试在此播放
+    }
+    
+    AudioServicesPlaySystemSound(shake_sound_male_id);   //播放注册的声音，（此句代码，可以在本类中的任意位置调用，不限于本方法中）
     
     //    AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);   //让手机震动
 }
